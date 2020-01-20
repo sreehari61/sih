5a7ee6fb241c4f58109099af9e1c9452c72be229
sreehari61 <39291275+sreehari61@users.noreply.github.com>
2020-01-20 22:35:30 +0530

    Add files via upload

---
+++ backend/config.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCGyhhyyxFZJpgdD49dLvd_hHJLCU4KR_w",
+    authDomain: "hackathon-841b3.firebaseapp.com",
+    databaseURL: "https://hackathon-841b3.firebaseio.com",
+    projectId: "hackathon-841b3",
+    storageBucket: "hackathon-841b3.appspot.com",
+    messagingSenderId: "1034008937152",
+    appId: "1:1034008937152:web:0bc69a2d22d5b2cb89a325",
+    measurementId: "G-5HLCJ28XQC"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
